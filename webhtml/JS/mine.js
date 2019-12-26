4e3e79f42a91891c1631927b36f45ea6139e74f9
hahaha <45685662+aaha-star@users.noreply.github.com>
2019-12-26 14:48:30 +0800

    Create mine.js

---
+++ webhtml/JS/mine.js
@@ -0,0 +1,32 @@
+ function openNav(){
+        document.getElementById("mySidenav").style.width = "250px";
+        }
+        function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        }
+        function savediary(){
+         var y=document.all.year.value;
+        var  m=document.all.month.value;
+        var d=document.all.day.value;
+        var diary="diaryname"+y+"."+m+"."+d;
+        document .getElementById("aaha").innerHTML =localStorage.getItem(diary);
+        }
+          function lookdiary(num){
+                document .getElementById("aaha").innerHTML =localStorage.getItem(localStorage.key(num));
+                }
+          function hrefweb(num){
+            if(localStorage.getItem(localStorage.key(num)).substring(0,4)!="http"){
+                 window.location.href="http://"+localStorage.getItem(localStorage.key(num));
+                          }
+                 else{
+                 window.location.href=localStorage.getItem(localStorage.key(num));
+                      }
+          }
+          function deletediary(num){
+                localStorage.removeItem(localStorage.key(num));
+                location.reload();
+                  }
+          function deleteweb(num){   
+        localStorage.removeItem(localStorage.key(num));
+        location.reload();
+        }
